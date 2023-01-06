0f64877f6f24ed7d979c036b4ca7d27df3b58045
moipo <moipochtoviyyashik@inbox.ru>
2023-01-06 13:16:39 +0300

    read data from json

--- project/app/api.py
+++ project/app/api.py
@@ -1,6 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from tinydb import TinyDB, Query
+
+
+
 class FormAPIView(APIView):
     def post(self, request):
+        db = TinyDB("db.json")
+        print(db.all())
+        # db.insert({"type":"apple", "count":"7"})
+        # print(db.all())
         return Response({"something":"something"})

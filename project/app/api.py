58f948fac635e29d23a1d20b0c32a23863f7f62a
moipo <moipochtoviyyashik@inbox.ru>
2023-01-06 21:22:09 +0300

    add utils

--- project/app/api.py
+++ project/app/api.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from tinydb import TinyDB, Query
-# from .utils import validate, get_type
+from .utils import get_type
 import json
 
 
@@ -12,8 +12,55 @@ class FormAPIView(APIView):
         Form = Query()
         data = request.data
 
-        print(db.search(Form.field_name_4.matches("[aZ]*")))
-        
-        var = "field_name_4"
-        print(db.search(getattr(Form, f'{var}').matches("[aZ]*")))
+
+
+        templates = []
+        for key in data.keys():
+            templates.extend(db.search(getattr(Form, f'{key}').matches("[aZ]*")))
+        templates = list(frozenset(templates))
+
+        print("TEMPLATES:" )
+        for template in templates:
+            print(template)
+        print()
+        # answers = []
+        fields_set = set(data.keys())
+        for template in templates:
+            print(set(template.keys()) , " in ", fields_set)
+            if set(template.keys()).issubset(fields_set):
+                print("is_subset")
+                for key in template.keys():
+                    if get_type(template[key])==type(data[key]):
+                        continue
+                    else:
+                        break
+                    return Response(template)
+
+
+
+        #performance optimization :
+        # template_set = set()
+        # # get all the templates with at least 1 matching name
+        # for key in data.keys():
+        #     template_set.update([str(i) for i in (db.search(getattr(Form, f'{key}').matches("[aZ]*")))])
+        #
+        #
+        # print(template_set)
+        #
+        # for possible_template_str in template_set:
+        #     possbile_template = json.loads(possible_template_str) #json
+        #     for field_name, value in possible_tempate:
+        #         if field_name in data.keys() and type(value)==type(data.get(field_name)):
+        #             continue
+        #         else:
+        #             break
+        #         print("answer")
+
+
+                # print(field_name, value)
+
+
+        # print(db.search(Form.field_name_4.matches("[aZ]*")))
+        # var = "field_name_4"
+        # print(db.search(getattr(Form, f'{var}').matches("[aZ]*")))
         return Response({"something":"something"})
--- project/app/utils.py
+++ project/app/utils.py
@@ -1,5 +1,37 @@
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
+import re
+import datetime
 
-# validate
-    # get_type
 
-# fields_match(dict1, dict2)
+
+def is_phone_number(phone_number):
+    match = re.search(r"\+7\s?[0-9]{3}\s?[0-9]{3}\s?[0-9]{2}\s?[0-9]{2}", phone_number)
+    if match and len(phone_number)==16: return True
+    else: return False
+
+def is_email(email):
+    try:
+        if validate_email() is None:
+            return True
+    except:
+        return False
+
+
+def is_date(date_text):
+        try:
+            datetime.datetime.strptime(date_text, '%Y-%m-%d')
+            return True
+        except:
+            try:
+                datetime.datetime.strptime(date_text, '%d.%m.%Y')
+                return True
+            except:
+                return False
+
+def get_type(data):
+    if is_phone_number(data): return "phone_number"
+    elif is_email(data): return "email"
+    elif is_date(data): return "date"
+    elif isinstance(data,str): return "text"
+    else: return "incorrect_input"
